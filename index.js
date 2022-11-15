39cf2e8eea42821209b7649e771bc52d7d79c340
Yash simejiya <107744977+Yashsoni23@users.noreply.github.com>
2022-11-15 13:15:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server,{
     cors:{
-        origin:"http://localhost:3000",
+        origin:"https://livechatapp.adaptable.app",
         methods:["GET","POST"]
     }
 })
