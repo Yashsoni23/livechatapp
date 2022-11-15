e81af90041412b58d0fbac495b996b28ce021bef
Yash simejiya <107744977+Yashsoni23@users.noreply.github.com>
2022-11-15 12:23:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const dotenv = require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const {Server} = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+const port = process.env.PORT || 8000;
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"]
+    }
+})
+io.on("connection", (socket) => {
+    // console.log(socket.id); // x8WIv7-mJelg7on_ALbx
+    socket.on("joinroom",room=>socket.join(room));
+    socket.on("newMessage",({newMessage,room})=>{
+        io.in(room).emit("getLatestMessage",newMessage);
+    });
+  });
+
+app.get("/",(req,res)=>{
+    res.send("socket chat BE started")
+})
+
+server.listen(port,()=>{
+    console.log(`app started at port ${port}`);
+})
