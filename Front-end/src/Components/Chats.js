b14ee599109761a2884356b290f008c83995eaf8
Yash simejiya <yashsoni48678@gmail.com>
2023-12-15 07:21:37 +0530

    height issue resolved

--- Front-end/src/Components/Chats.js
+++ Front-end/src/Components/Chats.js
@@ -30,13 +30,12 @@ const Chatroom = () => {
       });
     }
   }, [socket, allMessage]);
-  
 
   useEffect(() => {
     setData(location.state);
     // console.log(data);
   }, [location]);
-  const handleEnter = e=> e.keyCode === 13 ? onSubmit() : "";
+  const handleEnter = (e) => (e.keyCode === 13 ? onSubmit() : "");
 
   const onSubmit = () => {
     if (msg) {
@@ -59,17 +58,22 @@ const Chatroom = () => {
     <>
       <div className="container py-4 h-screen w-screen shadow bg-pink-300 text-dark border rounded">
         <div className="text-center px-3 mb-4 text-capitalize">
-          <h1 className="text-pink-700 font-bold text-2xl mb-4">{data?.room} Room</h1>
+          <h1 className="text-pink-700 font-bold text-2xl mb-4">
+            {data?.room} Room
+          </h1>
         </div>
         <div
           className="bg-pink-100 border chatscreen rounded p-3 mb-4 "
-          style={{ height: "525px", overflowY: "auto" }}
+          style={{ height: "80vh", overflowY: "auto" }}
         >
           {allMessage.map((msg) => {
             // console.log(msg);
             return data.name === msg.name ? (
               <>
-                <div className="justify-end float-right clear-both" key={msg.msg}>
+                <div
+                  className="justify-end float-right clear-both"
+                  key={msg.msg}
+                >
                   <div className=" flex-column float-right align-items-end m-2 shadow p-2 bg-pink-500 text-white border rounded w-auto">
                     <div>
                       <strong className="m-1">{msg.name}</strong>
@@ -96,27 +100,25 @@ const Chatroom = () => {
             );
           })}
           <div ref={refbox} className="divs"></div>
-
-          
         </div>
         <div className=" justify-center items-center d-flex">
-            <input
-              type="text bg-light"
-              className="h-max  w-full pl-4 py-1 font-semibold text-xl focus:outline-8 focus:border-4 rounded-lg focus:outline-none focus:border-pink-400 focus:outline-pink-400"
-              name="message"
-              placeholder="type your message ...."
-              value={msg}
-              onChange={handleChange}
-              onKeyDown={handleEnter}
-            />
-            <button
-              type="button"
-              className=" bg-pink-500 text-white p-2 rounded-xl hover:bg-pink-700 btn-danger  mx-2"
-              onClick={onSubmit}
-            >
-              Send
-            </button>
-          </div>
+          <input
+            type="text bg-light"
+            className="h-max  w-full pl-4 py-1 font-semibold text-xl focus:outline-8 focus:border-4 rounded-lg focus:outline-none focus:border-pink-400 focus:outline-pink-400"
+            name="message"
+            placeholder="type your message ...."
+            value={msg}
+            onChange={handleChange}
+            onKeyDown={handleEnter}
+          />
+          <button
+            type="button"
+            className=" bg-pink-500 text-white p-2 rounded-xl hover:bg-pink-700 btn-danger  mx-2"
+            onClick={onSubmit}
+          >
+            Send
+          </button>
+        </div>
       </div>
     </>
   );
